58d5976c66d9cf450db7b8586272d618424c2dd9
Christian Trutz <info@trutz-software-consulting.de>
2023-10-04 18:13:22 +0200

    feat: HibernateFailureAnalyzer erstellt

---
+++ failure-analyzer/src/main/java/trutzio/failureanalyzer/FailureAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package trutzio.failureanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FailureAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FailureAnalyzerApplication.class, args);
+    }
+
+}
